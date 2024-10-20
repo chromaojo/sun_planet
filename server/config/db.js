578a3114129bf7bb34715a30f58675a0f7fdcb15
Felix CHroma <oluwachroma@gmail,com>
2024-10-19 17:09:07 -0700

    Upload after the profile

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+// require('dotenv').config();
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASS,
-    database: process.env.DATABASE,
-    waitForConnections: true,
-  });
+// const db = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASS,
+//     database: process.env.DATABASE,
+//     waitForConnections: true,
+//   });
 
 // const db = mysql.createConnection({
   
@@ -19,13 +19,15 @@ const db = mysql.createConnection({
 
 // });
 
-
-// Workbench Setting 
-// host: 'localhost',
-// user: 'root',
-// password: 'Oluchroma234',
-// database: 'nica_app',
-// waitForConnections: true,
+const db = mysql.createConnection({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  uri: process.env.MYSQL_ADDON_URI,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database : process.env.MYSQL_ADDON_DB,
+  port : process.env.MYSQL_ADDON_PORT,
+  waitForConnections: true,
+});
 
 
 // const db = mysql.createPool({
